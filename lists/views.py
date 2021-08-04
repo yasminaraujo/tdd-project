df2b133119af01887d16e91ffa0b645991592e0c
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-04 11:08:20 -0300

    Ciclo: Teste de Unidade/Desenvolvimento

--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
