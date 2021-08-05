beaf2cc44cb0a42892e44715de1d8f9ec582c303
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-05 14:55:17 -0300

    Refatorando o Teste Funcional

--- functional_tests.py
+++ functional_tests.py
@@ -54,8 +54,27 @@ class NewVisitorTest(unittest.TestCase):
         # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
 
         # Ela visita a URL: a sua lista TODO ainda está armazenada
+        # Quando ela aperta enter, a página atualiza, e mostra a lista
+        # "1: Estudar testes funcionais" como um item da lista TODO
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox.send_keys('Estudar testes de unidade')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        self.check_for_row_in_list_table('2: Estudar testes de unidade')
         # Satisfeita, ela vai dormir
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
 if __name__ == '__main__':
     unittest.main()
