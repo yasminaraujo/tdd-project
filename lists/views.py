1eac86a8ca9bdd6626b6904555fbf7beceb041f3
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-05 13:27:41 -0300

    Refatorando a aplicação para utilizar templates

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
-# Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
