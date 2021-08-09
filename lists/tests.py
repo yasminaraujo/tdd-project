8742c9f758498b532ac31a930827ef9365600fdd
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-09 13:58:34 -0300

    Implementando o Novo Projeto: Ajustando o Modelo

--- lists/tests.py
+++ lists/tests.py
@@ -23,16 +23,24 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        my_list = List()
+        my_list.save()
+
         first_item = Item()
         first_item.text = 'O primeiro item'
+        first_item.list = my_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'O segundo item'
+        second_item.list = my_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, my_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -40,7 +48,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(first_saved_item.list, my_list)
         self.assertEqual(second_saved_item.text, 'O segundo item')
+        self.assertEqual(second_saved_item.list, my_list)
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
