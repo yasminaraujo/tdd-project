dae271bb078a861573b72e482207c0c5ee69919a
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-09 13:33:39 -0300

    Melhoria 1: Isolando Testes Funcionais

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -14,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self): 
     
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Ela nota que o título da página menciona TODO
         self.assertIn('To-Do', self.browser.title)
@@ -76,5 +76,5 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-if __name__ == '__main__':
-    unittest.main()
+
+unittest.main()
